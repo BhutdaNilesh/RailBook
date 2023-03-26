96a04aec6be7578838a99eb20fcf82fe7f1392d0
Suraj Patil <surajpatil5739@gmail.com>
2023-03-26 19:40:40 +0530

    added trains list in book ticket component

--- src/app/User/book-ticket/book-ticket.component.ts
+++ src/app/User/book-ticket/book-ticket.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class BookTicketComponent {
 
+  trains = [
+    {train_number : 100, train_name : "RAJDHANI EXPRESS", departure_time : "05:00 AM",origin : "PUNE", destination_time:"05:00 PM", destination:"NAGPUR", ac_seats: 50, sleeper_seats: 60, seater_seats: 70}
+  ]
+
+  
 }
