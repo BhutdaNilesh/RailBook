4a777c2b6f36732d6bfabaee1d99df743854aef6
Suraj Patil <surajpatil5739@gmail.com>
2023-03-28 15:49:11 +0530

    Book Ticket Form added

--- src/app/User/book-ticket/book-ticket.component.ts
+++ src/app/User/book-ticket/book-ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, NgIterable } from '@angular/core';
+
 
 @Component({
   selector: 'app-book-ticket',
@@ -8,8 +9,34 @@ import { Component } from '@angular/core';
 export class BookTicketComponent {
 
   trains = [
-    {train_number : 100, train_name : "RAJDHANI EXPRESS", departure_time : "05:00 AM",origin : "PUNE", destination_time:"05:00 PM", destination:"NAGPUR", ac_seats: 50, sleeper_seats: 60, seater_seats: 70}
+    {train_number : 1000003, train_name : "RAJDHANI EXPRESS", departure_time : "05:00 AM",origin : "PUNE", destination_time:"05:00 PM", destination:"NAGPUR", ac_seats: 50, sleeper_seats: 60, seater_seats: 70},
+    {train_number : 1000002, train_name : "SAHYADRI EXPRESS", departure_time : "05:00 AM",origin : "MUMBAI", destination_time:"05:00 PM", destination:"KOLHAPUR", ac_seats: 50, sleeper_seats: 60, seater_seats: 70}
   ]
 
-  
+
+  bookNow(){
+    
+    
+  }
+
+  j_Class : String = "AC";
+  no_Of_Passengers : number = 1;
+  passeneger_array: any  = [];
+
+  nextClicked:boolean = false;
+
+  nextButton(){
+    this.nextClicked= true;
+    this.passeneger_array = [];
+    for(let i=1; i<=this.no_Of_Passengers; i++){
+      this.passeneger_array.push(i);
+    }
+  }
+
+  close(){
+    this.nextClicked = false;
+    this.no_Of_Passengers = 1;
+  }
+
+  selectedValue: String = "";
 }
