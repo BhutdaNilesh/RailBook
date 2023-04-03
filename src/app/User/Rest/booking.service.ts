f44598cc7c62e43f01a2f0d1898bc2858deb0bb7
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2023-04-03 11:25:08 +0530

    added backend to my-booking

--- src/app/User/Rest/booking.service.ts
+++ src/app/User/Rest/booking.service.ts
@@ -2,20 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookingService {
-  API='http://localhost:9595';
+  API = 'http://localhost:9595';
 
-  constructor(private http:HttpClient) {
+  constructor(private http: HttpClient) {}
 
-  }
-
-  public getAllBooking(){
+  public getAllBooking() {
     return this.http.get(this.API + '/getAllBookings');
   }
 
-  public getPassenger(id : number){
-    return this.http.get(this.API + '/getPassenger/{id}');
+  public getPassenger(id: number) {
+    return this.http.get(this.API + '/getPasseneger/' + id);
+  }
+
+  public getTrain(id: number) {
+    return this.http.get(this.API + '/getTrain/' + id);
+  }
+
+  public deleteBooking(id:number){
+     return this.http.delete(this.API + '/deleteTicket/' + id)
   }
 }
--- src/app/User/my-bookings/my-bookings.component.ts
+++ src/app/User/my-bookings/my-bookings.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookingService } from '../Rest/booking.service';
 import { HttpClient } from '@angular/common/http';
 import { Booking } from '../Rest/booking';
@@ -8,83 +8,72 @@ import { Booking } from '../Rest/booking';
   templateUrl: './my-bookings.component.html',
   styleUrls: ['./my-bookings.component.css'],
 })
-export class MyBookingsComponent implements OnInit{
-  newBooking :Booking[] = [];
-  pass : any[] = [];
+export class MyBookingsComponent implements OnInit {
+  newBooking: Booking[] = [];
+  pass: any[] = [];
+  passData: any[] = [];
 
- constructor(private bookingServ : BookingService){}
+  train: any[] = [];
+
+  constructor(private bookingServ: BookingService) {}
 
   ngOnInit(): void {
-    this.bookingServ.getAllBooking().subscribe((resp:any)=>{
-      // console.log(resp);
-      this.newBooking = resp;
-    },
+    this.bookingServ.getAllBooking().subscribe(
+      (resp: any) => {
+        // console.log(resp);
+        this.newBooking = resp;
     
-    (err) => {
-      console.log(err);
-    });
-
-    for(let i=0; i<this.newBooking.length; i++){
-      let passlist = this.newBooking[i].passengerList;
-
-      console.log(i);
-      for(let j=0; j<passlist.length; j++){
-        this.bookingServ.getPassenger(passlist[j]).subscribe((resp:any)=>{
-          console.log(j);
-        })
+        for (let i = 0; i < this.newBooking.length; i++) {
+          let passlist = this.newBooking[i].passengerList;
+    
+          let trainId = this.newBooking[i].train_id;
+    
+          this.bookingServ.getTrain(trainId).subscribe((train_data: any) => {
+            this.train.push(train_data);
+          });
+    
+          let passData: any[] = []; // move the initialization here
+          // console.log(passlist);
+          for (let j = 0; j < passlist.length; j++) {
+            this.bookingServ
+              .getPassenger(passlist[j])
+              .subscribe((data: any) => {
+                passData.push(data); // push the passenger data here
+              });
+          }
+          this.pass.push(passData); // push the passData array here
+        }
+      },
+      
+      (err) => {
+        console.log(err);
       }
-    }
-    
-    
-  }
-  bookings = [
-    {
-      id: 1,
-      name: 'Nilesh',
-      age: 22,
-      gender: 'Male',
-      source: 'Nanded',
-      destination: 'Pune',
-      date: '2023-04-01',
-      train_name: 'Panvel Express',
-      cancelled: false,
-    },
-    {
-      id: 2,
-      name: 'Suraj',
-      age: 22,
-      gender: 'Male',
-      source: 'Pune',
-      destination: 'Kolhapur',
-      date: '2023-04-01',
-      train_name: 'Kolhapur Express',
-      cancelled: false,
-    },
-    {
-      id: 3,
-      name: 'Piyush',
-      age: 22,
-      gender: 'Male',
-      soruce: 'Pune',
-      destination: 'Dhule',
-      date: '2023-04-01',
-      train_name: 'Dhule Express',
-      cancelled: false,
-    },
-  ];
-
-
-  cancelBooking(id: number) {
-    const booking = this.bookings.find((b) => b.id === id);
-    if (booking) {
-      booking.cancelled = true;
-    }
+    );    
   }
 
   book: any = [];
-
-  view(booking: any) {
+  ind: number = 0;
+  view(booking: any, index: number) {
     this.book = booking;
+    this.ind = index;
+    // console.log(this.pass[this.ind])
+  }
+
+  cancelTicket() {
+    const bookingId = this.book.booking_id;
+    const confirmDelete = confirm(`Do you want to delete ticket with id: ${bookingId}?`);
+    if (confirmDelete) {
+      this.bookingServ.deleteBooking(bookingId).subscribe(
+        (resp: any) => {
+          // handle success response if needed
+          console.log(`Booking with id ${bookingId} was deleted successfully.`);
+        },
+        (err) => {
+          // handle error response if needed
+          console.log(`Error deleting booking with id ${bookingId}:`, err);
+        }
+      );
+    }
   }
   
 }
